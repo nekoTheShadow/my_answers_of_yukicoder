f9bcf356b086b70445882ea5069c686d3c2cb60d
Hajime Nakamura <h.nakamura0903@gmail.com>
2018-06-15 20:49:38 +0900

    rename some files with valid file names

--- admins/rename.sh
+++ admins/rename.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 find . -maxdepth 1 -type f | sed 's/^\.\///; s/\./ /g' | while read id ext; do
+  [[ ${#id} -eq 4 ]] && continue
   src="${id}.${ext}"
   dst=$(printf '%04d.%s' ${id} ${ext})
   mv -v ${src} ${dst}
