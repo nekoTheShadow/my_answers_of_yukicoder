7de78633e011ca69723b9333f570caf5ca6c9659
Hajime Nakamura <h.nakamura0903@gmail.com>
2018-06-29 21:13:11 +0900

    add 0548.cs

---
+++ 0548.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _548
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            var alphabets = "abcdefghijklm";
+            var cnt = s.Distinct().Count(ch => alphabets.Contains(ch));
+
+            if (cnt == 13)
+            {
+                foreach(var ch in alphabets)
+                {
+                    Console.WriteLine(ch);
+                }
+            }
+            else if (cnt == 12)
+            {
+                var ch = alphabets.First(alphabet => !s.Contains(alphabet));
+                Console.WriteLine(ch);
+            }
+            else
+            {
+                Console.WriteLine("Impossible");
+            }
+        }
+    }
+}
