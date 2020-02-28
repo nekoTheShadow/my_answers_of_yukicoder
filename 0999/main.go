c3b0c3aa99e13fc732b3cf1e46315a42a298d7f5
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-02-29 00:10:03 +0900

    yukicoder contest 239

---
+++ 0999/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func exec(stdin *Stdin, stdout *Stdout) {
+	n := stdin.ReadInt()
+	a := []int{}
+	for i := 0; i < 2*n; i++ {
+		a = append(a, stdin.ReadInt())
+	}
+
+	ans := 0
+	sum := 0
+	for i := 0; i < 2*n-1; i += 2 {
+		ans += a[i+1] - a[i]
+		sum += a[i+1] - a[i]
+	}
+
+	for i := 0; i < 2*n-1; i += 2 {
+		sum -= a[i+1] - a[i]
+		sum += a[i] - a[i+1]
+		ans = Max(ans, sum)
+	}
+
+	stdout.Println(ans)
+
+}
+
+func Max(a ...int) int {
+	v := a[0]
+	for i := 1; i < len(a); i++ {
+		if v < a[i] {
+			v = a[i]
+		}
+	}
+	return v
+}
+
+func main() {
+	stdout := NewStdout()
+	defer stdout.Flush()
+	exec(NewStdin(bufio.ScanWords), stdout)
+}
+
+type Stdin struct {
+	stdin *bufio.Scanner
+}
+
+func NewStdin(split bufio.SplitFunc) *Stdin {
+	s := Stdin{bufio.NewScanner(os.Stdin)}
+	s.stdin.Split(split)
+	s.stdin.Buffer(make([]byte, bufio.MaxScanTokenSize), int(math.MaxInt32))
+	return &s
+}
+
+func (s *Stdin) Read() string {
+	s.stdin.Scan()
+	return s.stdin.Text()
+}
+
+func (s *Stdin) ReadInt() int {
+	n, _ := strconv.Atoi(s.Read())
+	return n
+}
+
+func (s *Stdin) ReadFloat64() float64 {
+	n, _ := strconv.ParseFloat(s.Read(), 64)
+	return n
+}
+
+type Stdout struct {
+	stdout *bufio.Writer
+}
+
+func NewStdout() *Stdout {
+	return &Stdout{bufio.NewWriter(os.Stdout)}
+}
+
+func (s *Stdout) Flush() {
+	s.stdout.Flush()
+}
+
+func (s *Stdout) Println(a ...interface{}) {
+	fmt.Fprintln(s.stdout, a...)
+}
