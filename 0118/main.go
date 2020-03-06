5d77082e52807ad12d5cce6c4bc5a40408541773
nekoTheShadow <h.nakamura0903@gmail.com>
2020-03-06 13:24:19 +0900

    No.118 門松列(2)

---
+++ 0118/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func exec(stdin *Stdin, stdout *Stdout) {
+	n := stdin.ReadInt()
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = stdin.ReadInt()
+	}
+
+	c := make([]int, 100)
+	for _, v := range a {
+		c[v-1]++
+	}
+
+	ans := 0
+	for i := 0; i < 100; i++ {
+		for j := i + 1; j < 100; j++ {
+			for k := j + 1; k < 100; k++ {
+				ans += c[i] * c[j] * c[k] % 1000000007
+				ans %= 1000000007
+			}
+		}
+	}
+
+	stdout.Println(ans)
+}
+
+func main() {
+	stdout := NewStdout()
+	defer stdout.Flush()
+	exec(NewStdin(bufio.ScanWords), stdout)
+}
+
+type Stdin struct {
+	stdin *bufio.Scanner
+}
+
+func NewStdin(split bufio.SplitFunc) *Stdin {
+	s := Stdin{bufio.NewScanner(os.Stdin)}
+	s.stdin.Split(split)
+	s.stdin.Buffer(make([]byte, bufio.MaxScanTokenSize), int(math.MaxInt32))
+	return &s
+}
+
+func (s *Stdin) Read() string {
+	s.stdin.Scan()
+	return s.stdin.Text()
+}
+
+func (s *Stdin) ReadInt() int {
+	n, _ := strconv.Atoi(s.Read())
+	return n
+}
+
+func (s *Stdin) ReadFloat64() float64 {
+	n, _ := strconv.ParseFloat(s.Read(), 64)
+	return n
+}
+
+type Stdout struct {
+	stdout *bufio.Writer
+}
+
+func NewStdout() *Stdout {
+	return &Stdout{bufio.NewWriter(os.Stdout)}
+}
+
+func (s *Stdout) Flush() {
+	s.stdout.Flush()
+}
+
+func (s *Stdout) Println(a ...interface{}) {
+	fmt.Fprintln(s.stdout, a...)
+}
