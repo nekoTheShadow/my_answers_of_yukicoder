db4d2dd97d94df69f00d1554ce4bcbce7bc883b1
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-03-07 13:37:17 +0900

    No.1005 BOT対策

---
+++ 1005/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func exec(stdin *Stdin, stdout *Stdout) {
+	s := stdin.Read()
+	t := stdin.Read()
+
+	if len(t) == 1 {
+		if strings.Count(s, t) == 0 {
+			stdout.Println(0)
+		} else {
+			stdout.Println(-1)
+		}
+		return
+	}
+
+	c := 0
+	for {
+		x := strings.Index(s, t)
+		if x == -1 {
+			break
+		}
+		c++
+		s = s[x:x+len(t)-1] + "." + s[x+len(t)-1:]
+	}
+	stdout.Println(c)
+}
+
+func main() {
+	stdout := NewStdout()
+	defer stdout.Flush()
+	exec(NewStdin(bufio.ScanWords), stdout)
+}
+
+type Stdin struct {
+	stdin *bufio.Scanner
+}
+
+func NewStdin(split bufio.SplitFunc) *Stdin {
+	s := Stdin{bufio.NewScanner(os.Stdin)}
+	s.stdin.Split(split)
+	s.stdin.Buffer(make([]byte, bufio.MaxScanTokenSize), int(math.MaxInt32))
+	return &s
+}
+
+func (s *Stdin) Read() string {
+	s.stdin.Scan()
+	return s.stdin.Text()
+}
+
+func (s *Stdin) ReadInt() int {
+	n, _ := strconv.Atoi(s.Read())
+	return n
+}
+
+func (s *Stdin) ReadFloat64() float64 {
+	n, _ := strconv.ParseFloat(s.Read(), 64)
+	return n
+}
+
+type Stdout struct {
+	stdout *bufio.Writer
+}
+
+func NewStdout() *Stdout {
+	return &Stdout{bufio.NewWriter(os.Stdout)}
+}
+
+func (s *Stdout) Flush() {
+	s.stdout.Flush()
+}
+
+func (s *Stdout) Println(a ...interface{}) {
+	fmt.Fprintln(s.stdout, a...)
+}
