fd48029642b232bde881ecfa82cb2647f2531fe2
nekoTheShadow <h.nakamura0903@gmail.com>
2020-07-09 14:35:03 +0900

    No.1091 Range Xor Query

---
+++ _1091/Main.java
@@ -0,0 +1,110 @@
+package _1091;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        int n = stdin.nextInt();
+        int q = stdin.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = stdin.nextInt();
+
+        int[] d = new int[n+1];
+        for (int i = 0; i < n; i++) d[i+1] = d[i] ^ a[i];
+
+        for (int i = 0; i < q; i++) {
+            int l = stdin.nextInt() - 1;
+            int r = stdin.nextInt() - 1;
+            stdout.println(d[l] ^ d[r+1]);
+        }
+    }
+
+    private static final Stdin stdin = new Stdin();
+    private static final Stdout stdout = new Stdout();
+
+    public static void main(String[] args) {
+        new Main().exec();
+        stdout.flush();
+    }
+
+    public static class Stdin {
+        private BufferedReader stdin;
+        private Deque<String> tokens;
+        private Pattern delim;
+
+        public Stdin() {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            tokens = new ArrayDeque<>();
+            delim = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            try {
+                if (tokens.isEmpty()) {
+                    String line = stdin.readLine();
+                    delim.splitAsStream(line).forEach(tokens::addLast);
+                }
+                return tokens.pollFirst();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout() {
+            stdout =  new PrintWriter(System.out, false);
+        }
+
+        public void printf(String format, Object ... args) {
+            stdout.printf(format, args);
+        }
+
+        public void println(Object ... objs) {
+            String line = Arrays.stream(objs).map(this::deepToString).collect(Collectors.joining(" "));
+            stdout.println(line);
+        }
+
+        private String deepToString(Object o) {
+            if (o == null || !o.getClass().isArray()) {
+                return Objects.toString(o);
+            }
+
+            int len = Array.getLength(o);
+            String[] tokens = new String[len];
+            for (int i = 0; i < len; i++) {
+                tokens[i] = deepToString(Array.get(o, i));
+            }
+            return "{" + String.join(",", tokens) + "}";
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
