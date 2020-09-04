3405fbe8c14d0a3dce816080d5acee918bde731c
nekoTheShadow <h.nakamura0903@gmail.com>
2020-09-04 13:20:11 +0900

    No.1136 Four Points Tour

---
+++ _1136/Main.java
@@ -0,0 +1,164 @@
+package _1136;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        var s = stdin.nextString();
+        if (new BigInteger(s).compareTo(BigInteger.TEN.pow(6)) == 1) {
+            stdout.println("igore");
+            return;
+        }
+
+        int n = Integer.parseInt(s);
+        long[][] dp = new long[n+1][4];
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (j == k) continue;
+                    dp[i+1][k] += dp[i][j];
+                    dp[i+1][k] %= 1000000000 + 7;
+                }
+            }
+        }
+
+        stdout.println(dp[n][0]);
+    }
+
+    private static final Stdin stdin = new Stdin();
+    private static final Stdout stdout = new Stdout();
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private BufferedReader stdin;
+        private Deque<String> tokens;
+        private Pattern delim;
+
+        public Stdin() {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            tokens = new ArrayDeque<>();
+            delim = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            try {
+                if (tokens.isEmpty()) {
+                    String line = stdin.readLine();
+                    if (line == null) {
+                        throw new UncheckedIOException(new EOFException());
+                    }
+                    delim.splitAsStream(line).forEach(tokens::addLast);
+                }
+                return tokens.pollFirst();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout() {
+            stdout =  new PrintWriter(System.out, false);
+        }
+
+        public void printf(String format, Object ... args) {
+            String line = String.format(format, args);
+            if (line.endsWith(System.lineSeparator())) {
+                stdout.print(line);
+            } else {
+                stdout.println(line);
+            }
+        }
+
+        public void println(Object ... objs) {
+            String line = Arrays.stream(objs).map(Objects::toString).collect(Collectors.joining(" "));
+            stdout.println(line);
+        }
+
+        public void printDebug(Object ... objs) {
+            String line = Arrays.stream(objs).map(this::deepToString).collect(Collectors.joining(" "));
+            stdout.printf("DEBUG: %s%n", line);
+            stdout.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(",", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+
+        private void flush() {
+            stdout.flush();
+        }
+    }
+}
