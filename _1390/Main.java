251f5215ba7501ed51556bf0d5155cada9a240a6
hajime nakamura <h.nakamura0903@gmail.com>
2021-07-23 22:03:26 +0900

    No.1390 Get together

---
+++ _1390/Main.java
@@ -0,0 +1,234 @@
+package _1390;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class Main {
+    public void exec() {
+        int n = stdin.nextInt();
+        int m = stdin.nextInt();
+        
+        int[] b = new int[n];
+        int[] c = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = stdin.nextInt();
+            c[i] = stdin.nextInt();
+        }
+        
+        Map<Integer, List<Integer>> d = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            d.computeIfAbsent(b[i], unused -> new ArrayList<>()).add(c[i]);
+        }
+        
+        Map<Integer, Integer> e = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            e.put(c[i], b[i]);
+        }
+        
+        UnionFind uf = new UnionFind(Arrays.stream(c).max().getAsInt()+1);
+        for (List<Integer> slimes : d.values()) {
+            for (int i = 1; i < slimes.size(); i++) {
+                uf.union(slimes.get(0), slimes.get(i));
+            }
+        }
+        
+        int count = 0;
+        for (int box : d.keySet()) {
+            List<Integer> slimes = d.get(box);
+            if (e.get(uf.find(slimes.get(0))) != box) {
+                count++;
+            }
+        }
+        stdout.println(count);
+    }
+    
+    public class UnionFind {
+        private int[] parent;
+        private int[] size;
+
+        public UnionFind(int n) {
+            this.parent = IntStream.range(0, n).toArray();
+            this.size = new int[n];
+            Arrays.fill(this.size, 1);
+        }
+
+        public int find(int x) {
+            if (parent[x] == x) {
+                return x;
+            }
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        public void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x == y) {
+                return ;
+            }
+
+            if (size[x] < size[y]) {
+                parent[x] = y;
+                size[y] += size[x];
+            } else {
+                parent[y] = x;
+                size[x] += size[y];
+            }
+
+        }
+
+        public int size(int x) {
+            return size[find(x)];
+        }
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        
+        public BigInteger[] nexBigIntegerArray(int n) {
+            BigInteger[] a = new BigInteger[n];
+            for (int i = 0; i < n; i++) a[i] = nextBigInteger();
+            return a;
+        }
+        
+        public List<Integer> nextIntegerList(int n) {
+            return nextList(n, this::nextInt);
+        }
+        
+        public List<Long> nextLongList(int n) {
+            return nextList(n, this::nextLong);
+        }
+        
+        public List<Double> nextDoubleList(int n) {
+            return nextList(n, this::nextDouble);
+        }
+        
+        public List<String> nextStringList(int n) {
+            return nextList(n, this::nextString);
+        }
+        
+        public List<BigInteger> nextBigIntegerList(int n) {
+            return nextList(n, this::nextBigInteger);
+        }
+        
+        private <T> List<T> nextList(int n, Supplier<T> supplier) {
+            List<T> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) a.add(supplier.get());
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
