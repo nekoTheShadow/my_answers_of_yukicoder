1abc489c1c2e01d947e8e6f5a523d20d92bc77cc
hajime nakamura <h.nakamura0903@gmail.com>
2021-07-23 22:03:36 +0900

    No.1473 おでぶなおばけさん

---
+++ _1473/Main.java
@@ -0,0 +1,207 @@
+package _1473;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+public class Main {
+    public void exec() {
+        int n = stdin.nextInt();
+        int m = stdin.nextInt();
+        Map<Integer, List<Edge>> edges = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            int s = stdin.nextInt()-1;
+            int t = stdin.nextInt()-1;
+            int d = stdin.nextInt();
+            edges.computeIfAbsent(s, unused -> new ArrayList<>()).add(new Edge(t, d));
+            edges.computeIfAbsent(t, unused -> new ArrayList<>()).add(new Edge(s, d));
+        }
+        
+        int ok = 0;
+        int ng = 1000000000+1;
+        int ans = 0;
+        while (Math.abs(ok-ng)>1) {
+            int mi = (ok+ng)/2;
+
+            
+            Deque<Integer> stack = new ArrayDeque<>();
+            int[] count = new int[n];
+            stack.add(0);
+            Arrays.fill(count, Integer.MAX_VALUE);
+            count[0]=0;
+
+            while (!stack.isEmpty()) {
+                int cur = stack.pop();
+                for (Edge e : edges.get(cur)) {
+                    if (mi <= e.d && count[cur]+1 < count[e.next]) {
+                        stack.add(e.next);
+                        count[e.next] = count[cur]+1;
+                    }
+                }
+            }
+            if (count[n-1]!=Integer.MAX_VALUE) {
+                ok = mi;
+                ans = count[n-1];
+            } else {
+                ng = mi;
+            }
+        }
+        
+        stdout.println(ok, ans);
+    }
+    
+    public class Edge {
+        public int next;
+        public int d;
+        public Edge(int next, int d) {
+            super();
+            this.next = next;
+            this.d = d;
+        }
+    }
+    
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        
+        public BigInteger[] nexBigIntegerArray(int n) {
+            BigInteger[] a = new BigInteger[n];
+            for (int i = 0; i < n; i++) a[i] = nextBigInteger();
+            return a;
+        }
+        
+        public List<Integer> nextIntegerList(int n) {
+            return nextList(n, this::nextInt);
+        }
+        
+        public List<Long> nextLongList(int n) {
+            return nextList(n, this::nextLong);
+        }
+        
+        public List<Double> nextDoubleList(int n) {
+            return nextList(n, this::nextDouble);
+        }
+        
+        public List<String> nextStringList(int n) {
+            return nextList(n, this::nextString);
+        }
+        
+        public List<BigInteger> nextBigIntegerList(int n) {
+            return nextList(n, this::nextBigInteger);
+        }
+        
+        private <T> List<T> nextList(int n, Supplier<T> supplier) {
+            List<T> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) a.add(supplier.get());
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
