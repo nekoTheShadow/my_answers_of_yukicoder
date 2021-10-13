88982a64d6f31187997d3bc2cc4d0f409d204824
neko_the_shadow <h.nakamura0903@gmail.com>
2021-10-13 19:35:13 +0900

    No.1674 Introduction to XOR

--- 1674.pl
+++ 1674.pl
@@ -1,35 +1,13 @@
 use strict;
 use warnings;
-use feature qw/state/;
+use List::Util qw(first);
 
-sub output {
-    print "@_\n";
-}
+my $n = <>;
+my @a = split / /, <>;
 
-sub input {
-    state @que;
-    if (@que == 0) {
-        chomp(my $line = <>);
-        push @que, split / /, $line;
-    }
-    return shift @que;
-}
-
-sub main {
-    my $n = input;
-    my $q = input;
-
-    my @d = (0) x $n;
-    my $c = 0;
-    foreach (1..$q) {
-        my $l = input - 1;
-        my $r = input - 1;
-        foreach my $i ($l..$r) {
-            $d[$i] = abs($d[$i]-1);
-            $d[$i]==0 ? $c-- : $c++;
-        }
-        output $c;
+for (my $x = 1; ; $x *= 2) {
+    unless (first {$_ & $x} @a) {
+        print "$x\n";
+        exit;
     }
 }
-
-main;
